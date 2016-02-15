5bc68f278c680a89c941560c3907b1b315ac2925
Hijiri Home <hijiribarrett@gmail.com>
2016-02-15 18:41:59 +0000

    began strategy pattern exercise

---
+++ javaExercises/src/week5/strategy/TestStrategyPattern.java
@@ -0,0 +1,17 @@
+package week5.strategy;
+
+/**
+ * Created by Hijiri on 15/02/2016.
+ */
+public class TestStrategyPattern {
+
+    public static void main(String[] args) {
+
+        TextFormatter formatter = new CapTextFormatter();
+        TextEditor editor = new TextEditor(formatter);
+        editor.publishText("Testing text in caps formatter");
+        formatter = new LowerTextFormatter();
+        editor = new TextEditor(formatter);
+        editor.publishText("Testing text in lower formatter");
+    }
+}
---
+++ javaExercises/src/week5/strategy/TextFormatter.java
@@ -0,0 +1,8 @@
+package week5.strategy;
+
+/**
+ * Created by Hijiri on 15/02/2016.
+ */
+public interface TextFormatter {
+    void format(String text);
+}
