a5ab78a5ef594484780ab70505c4de586e2a2de5
Hijiri Home <hijiribarrett@gmail.com>
2016-02-16 21:53:33 +0000

    completed first part of class arguments

---
+++ scalaExercises/src/week6/classArguments/Family.scala
@@ -0,0 +1,9 @@
+package week6.classArguments
+
+/**
+  * Created by Hijiri on 16/02/2016.
+  */
+class Family (family : String*) {
+
+  def familySize(): Int = { family.length }
+}
---
+++ scalaExercises/src/week6/classArguments/TestArgs.scala
@@ -0,0 +1,16 @@
+package week6.classArguments
+
+import atomic.AtomicTest._
+
+/**
+  * Created by Hijiri on 16/02/2016.
+  */
+object TestArgs extends App {
+
+  val family1 = new Family("Mum", "Dad", "Sally", "Dick")
+  family1.familySize() is 4
+
+  val family2 = new Family("Dad", "Mom", "Harry")
+  family2.familySize() is 3
+}
+
