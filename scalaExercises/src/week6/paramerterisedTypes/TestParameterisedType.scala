06ba37cfc2b955970a1fad3be756c2ebf066bfbd
Hijiri Home <hijiribarrett@gmail.com>
2016-02-18 01:00:58 +0000

    something

---
+++ scalaExercises/src/week6/paramerterisedTypes/TestParameterisedType.scala
@@ -0,0 +1,31 @@
+package week6.paramatisedTypes
+
+import atomic.AtomicTest._
+
+/**
+  * Created by Hijiri on 17/02/2016.
+  */
+object TestParameterisedType extends App {
+
+  // Return type is inferred:
+  def inferred(c1: Char, c2: Char, c3: Char) = {
+    Vector(c1, c2, c3)
+  }
+  // Explicit return type:
+  def explicit(c1: Char, c2: Char, c3: Char): Vector[Char] = {
+    Vector(c1, c2, c3)
+  }
+
+  def explicitDouble(d1: Double, d2: Double, d3: Double): Vector[Double] = {
+    Vector(d1, d2, d3)
+  }
+
+  inferred('a', 'b', 'c') is "Vector(a, b, c)"
+  explicit('a', 'b', 'c') is "Vector(a, b, c)"
+  explicitDouble(1.0, 2.0, 3.0) is Vector(1.0, 2.0, 3.0)
+
+  explicitList(Vector(10.0, 20.0)) is List(10.0, 20.0)
+  explicitList(Vector(1, 2, 3)) is List(1.0, 2.0, 3.0)
+
+
+}
