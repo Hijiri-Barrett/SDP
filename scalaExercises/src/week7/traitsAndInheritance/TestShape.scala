3af68cd6f6b4d978b4ff9f05364a8a233f28603f
Hijiri Home <hijiribarrett@gmail.com>
2016-02-20 17:34:45 +0000

    created Draw object

--- scalaExercises/src/week7/traitsAndInheritance/TestShape.scala
+++ scalaExercises/src/week7/traitsAndInheritance/TestShape.scala
@@ -5,6 +5,12 @@ package week7.traitsAndInheritance
   * Created by Hijiri on 20/02/2016.
   */
 
+object Draw {
+
+  def apply(shape:Shape): Unit = {
+    shape.toString
+  }
+}
 
 object TestShape {
 
