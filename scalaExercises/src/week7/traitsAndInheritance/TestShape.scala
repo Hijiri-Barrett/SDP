6ee8cd366ea44b0711b13cee02f43f51bac8b6e9
Hijiri Home <hijiribarrett@gmail.com>
2016-02-20 17:26:32 +0000

    moved things around

--- scalaExercises/src/week7/traitsAndInheritance/TestShape.scala
+++ scalaExercises/src/week7/traitsAndInheritance/TestShape.scala
@@ -5,34 +5,6 @@ package week7.traitsAndInheritance
   * Created by Hijiri on 20/02/2016.
   */
 
-trait Shape {
-
-  def sides : Int
-  def perimeter : Double
-  def area : Double
-}
-
-class Circle (radius:Double)extends Shape {
-
-  override def sides: Int = 1
-
-  override def perimeter: Double = 2*radius*math.Pi
-
-  override def area: Double = math.Pi * radius * radius
-}
-
-abstract class Rectangular (length:Double, height:Double) extends Shape {
-
-  override def sides: Int = 4
-
-  override def perimeter: Double = 2*length + 2*height
-
-  override def area: Double = length*height
-}
-
-class Rectangle (length:Double, height:Double) extends Rectangular (length, height)
-
-class Square (length:Double) extends Rectangular (length, length)
 
 object TestShape {
 
