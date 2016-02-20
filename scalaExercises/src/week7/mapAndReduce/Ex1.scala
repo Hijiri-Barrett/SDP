894771f186bf8014504bcfc0713c5f39fc88f816
Hijiri Home <hijiribarrett@gmail.com>
2016-02-20 16:41:17 +0000

    started week 7, completed ex1

--- scalaExercises/src/week7/mapAndReduce/Ex1.scala
+++ scalaExercises/src/week7/mapAndReduce/Ex1.scala
@@ -13,4 +13,17 @@ object Ex1 extends App{
   val v2 = Vector(1, 2, 3, 4)
   v2.map(n => 11 * n + 10) is Vector(21, 32, 43, 54)
 
+  val v3 = Vector(1, 2, 3, 4)
+  v3.foreach(n => 11 * n + 10) is Vector(21, 32, 43, 54)
+
+  // for each does not return a Vector but instead just runs the
+  // function for each element.
+
+  val v4 = Vector(1, 2, 3, 4)
+  for(n <- 0 to 3) {
+    v4.updated(n, 2*n) //(11 * n + 10)
+  }
+  v4 is Vector(21, 32, 43, 54)
+
+  //screw vectors
 }
