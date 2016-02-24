f0a51a316791426dc4cfffcc52e08c43767a24a7
Hijiri Home <hijiribarrett@gmail.com>
2016-02-24 16:22:54 +0000

    edited BnzInstruction to use reflection to get the label field

--- cw-one/src/sml/BnzInstruction.scala
+++ cw-one/src/sml/BnzInstruction.scala
@@ -7,28 +7,33 @@ class BnzInstruction (label: String, op: String, val r: Int, label2: String)
   extends Instruction (label, op) {
 
   override def execute(m: Machine) {
-    val value = m.regs(r)
 
-    if(value==0) {
-      for(inst <- m.prog) {
-        val method = inst.getClass.getDeclaredField("label")
-        val instLabel = method.get(inst).asInstanceOf[String]
+    if(m.regs(r)==0) {
+      import scala.reflect.runtime.{universe => ru}
+      val mirror = ru.runtimeMirror(classOf[Instruction].getClassLoader)
 
-        if(instLabel == label2) {
-          inst.execute(m)
+      for(instruction <- m.prog) {
+        val labelTermSymb = ru.typeOf[Instruction].decl(ru.TermName("label")).asTerm
+        val im = mirror.reflect(instruction)
+        val labelMirror = im.reflectField(labelTermSymb)
+
+        if(labelMirror.get == label2) {
+          instruction.execute(m)
           return
         }
       }
     }
+    else { return }
+
     println(label2 + " does not exist.")
   }
 
   override def toString(): String = {
-    super.toString + " If " + r + " == 0: run " + label2 + "\n"
+    super.toString + " if " + r + " == 0 -> run " + label2 + "\n"
   }
 }
 
 object BnzInstruction {
-  def apply(label1: String, op1: Int, label2: String) =
-    new BnzInstruction(label1, "bnz", op1, label2)
+  def apply(label1: String, r: Int, label2: String) =
+    new BnzInstruction(label1, "bnz", r, label2)
 }
--- cw-one/src/test2.sml
+++ cw-one/src/test2.sml
@@ -4,6 +4,8 @@ f2 lin 22 1
 f3 mul 21 21 20
 f4 sub 20 20 22
 f5 bnz 20 f3
-f6 out 21
-f7 add 1 20 21
-f8 div 3 1 20
+f6 bnz 0 f3
+f7 out 21
+f8 add 1 20 21
+f9 div 3 1 20
+f10 bnz 0 f930
