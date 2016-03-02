21e4cb57b4c06867180ac72ad0318df6185bd15e
Hijiri Home <hijiribarrett@gmail.com>
2016-03-02 13:51:31 +0000

    Finished reflection and its not very good

--- cw-one/src/sml/Translator.scala
+++ cw-one/src/sml/Translator.scala
@@ -4,13 +4,7 @@ package sml
  * The translator of a <b>S</b><b>M</b>al<b>L</b> program.
  */
 class Translator(fileName: String) {
-  private final val ADD = "add"
-  private final val LIN = "lin"
-  private final val MUL = "mul"
-  private final val SUB = "sub"
-  private final val DIV = "div"
-  private final val OUT = "out"
-  private final val BNZ = "bnz"
+  private final val PACKAGE = "sml."
 
   // word + line is the part of the current line that's not yet processed
   // word has no whitespace
@@ -28,27 +22,47 @@ class Translator(fileName: String) {
       val fields = line.split(" ")
       if (fields.length > 0) {
         labels.add(fields(0))
-        fields(1) match {
-          case ADD =>
-            program = program :+ AddInstruction(fields(0), fields(2).toInt, fields(3).toInt, fields(4).toInt)
-          case LIN =>
-            program = program :+ LinInstruction(fields(0), fields(2).toInt, fields(3).toInt)
-          case MUL =>
-            program = program :+ MulInstruction(fields(0), fields(2).toInt, fields(3).toInt, fields(4).toInt)
-          case SUB =>
-            program = program :+ SubInstruction(fields(0), fields(2).toInt, fields(3).toInt, fields(4).toInt)
-          case DIV =>
-            program = program :+ DivInstruction(fields(0), fields(2).toInt, fields(3).toInt, fields(4).toInt)
-          case OUT =>
-            program = program :+ OutInstruction(fields(0), fields(2).toInt)
-          case BNZ =>
-            program = program :+ BnzInstruction(fields(0), fields(2).toInt, fields(3))
+
+
+        val instructionClass = Class.forName(PACKAGE + fields(1).capitalize + "Instruction")
+        val constructors = instructionClass.getConstructors
+        val dependencies = getDependencies(constructors(0).getParameterTypes, fields)
+
+        dependencies.length match {
+          case 2 =>
+            program = program :+ constructors(0).newInstance(
+              dependencies(0), dependencies(1)).asInstanceOf[Instruction]
+          case 3 =>
+            program = program :+ constructors(0).newInstance(
+              dependencies(0), dependencies(1), dependencies(2)).asInstanceOf[Instruction]
+          case 4 =>
+            program = program :+ constructors(0).newInstance(
+              dependencies(0), dependencies(1), dependencies(2), dependencies(3)).asInstanceOf[Instruction]
+          case 5 =>
+            program = program :+ constructors(0).newInstance(
+              dependencies(0), dependencies(1), dependencies(2), dependencies(3), dependencies(4)).asInstanceOf[Instruction]
           case x =>
-            println(s"Unknown instruction $x")
+            println("Unknown Instruction " + fields(1))
         }
       }
     }
     new Machine(labels, program)
+  }
+
+  private def getDependencies(paramTypes: Array[Class[_]], fields: Array[String]): Array[Object] = {
+
+    val size = fields.length
+    val result : Array[Any] = new Array(size)
+    for(i <- 0 until size) {
+
+      if(paramTypes(i) == classOf[Int]) {
+        val value : Any = fields(i).toInt
+        result(i) = value
+      } else {
+        result(i) = fields(i)
+      }
+    }
+    result.asInstanceOf[Array[Object]]
   }
 }
 
